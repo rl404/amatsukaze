8fa06f6e159a580e2039c6b0d54bdcf97a4a030d
axel <axel.rl.404@gmail.com>
2023-02-24 10:44:19 +0700

    init

--- src/lib/component/Divider.svelte
+++
@@ -1,8 +0,0 @@
-<div class="relative">
-	<div class="absolute inset-0 flex items-center">
-		<div class="w-full border-b border-gray-200" />
-	</div>
-	<div class="relative flex justify-center">
-		<span class="bg-white px-2"><slot /></span>
-	</div>
-</div>
