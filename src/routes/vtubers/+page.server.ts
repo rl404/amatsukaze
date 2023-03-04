c440b14d9d3e7dd50130f52673d46ffd92da71a1
axel <axel.rl.404@gmail.com>
2023-03-04 10:22:34 +0700

    feat: infinity scroll

--- src/routes/vtubers/+page.server.ts
+++ src/routes/vtubers/+page.server.ts
@@ -1,13 +1,5 @@
 import { PUBLIC_SHIMAKAZE_HOST } from '$env/static/public';
-import type { vtuberResponseData } from '../api/vtubers/[id]/+server';
 import type { PageServerLoad } from './$types';
-
-export type vtubersResponse = {
-	status: number;
-	message: string;
-	data: Array<vtuberResponseData>;
-	meta: { page: number; limit: number; total: number };
-};
 
 export const load = (async () => {
 	const resp = await fetch(`${PUBLIC_SHIMAKAZE_HOST}/vtubers?page=1&limit=24`);
