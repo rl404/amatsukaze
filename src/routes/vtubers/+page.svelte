c8f55bf5ac9155b2d68f06956a698bfe4705a17f
axel <axel.rl.404@gmail.com>
2023-03-05 10:43:32 +0700

    refactor: split layout component

--- src/routes/vtubers/+page.svelte
+++ src/routes/vtubers/+page.svelte
@@ -10,34 +10,16 @@
 	import type { vtuberResponseData } from '../api/vtubers/[id]/+server';
 	import axios from 'axios';
 	import InputSearch from './InputSearch.svelte';
+	import AdvancedSearch from './AdvancedSearch.svelte';
+	import Layout from './Layout.svelte';
 
 	export let data: vtubersResponse;
-
-	const layouts = [
-		{
-			name: 'grid',
-			svg: 'M3 6a3 3 0 013-3h2.25a3 3 0 013 3v2.25a3 3 0 01-3 3H6a3 3 0 01-3-3V6zm9.75 0a3 3 0 013-3H18a3 3 0 013 3v2.25a3 3 0 01-3 3h-2.25a3 3 0 01-3-3V6zM3 15.75a3 3 0 013-3h2.25a3 3 0 013 3V18a3 3 0 01-3 3H6a3 3 0 01-3-3v-2.25zm9.75 0a3 3 0 013-3H18a3 3 0 013 3V18a3 3 0 01-3 3h-2.25a3 3 0 01-3-3v-2.25z'
-		},
-		{
-			name: 'card',
-			svg: 'M4.5 3.75a3 3 0 00-3 3v10.5a3 3 0 003 3h15a3 3 0 003-3V6.75a3 3 0 00-3-3h-15zm4.125 3a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5zm-3.873 8.703a4.126 4.126 0 017.746 0 .75.75 0 01-.351.92 7.47 7.47 0 01-3.522.877 7.47 7.47 0 01-3.522-.877.75.75 0 01-.351-.92zM15 8.25a.75.75 0 000 1.5h3.75a.75.75 0 000-1.5H15zM14.25 12a.75.75 0 01.75-.75h3.75a.75.75 0 010 1.5H15a.75.75 0 01-.75-.75zm.75 2.25a.75.75 0 000 1.5h3.75a.75.75 0 000-1.5H15z'
-		},
-		{
-			name: 'list',
-			svg: 'M2.625 6.75a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875 0A.75.75 0 018.25 6h12a.75.75 0 010 1.5h-12a.75.75 0 01-.75-.75zM2.625 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zM7.5 12a.75.75 0 01.75-.75h12a.75.75 0 010 1.5h-12A.75.75 0 017.5 12zm-4.875 5.25a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875 0a.75.75 0 01.75-.75h12a.75.75 0 010 1.5h-12a.75.75 0 01-.75-.75z'
-		}
-	];
-
-	let layout: number = 0;
-
-	const nextLayout = () => {
-		layout = (layout + 1) % layouts.length;
-	};
 
 	let name = '';
 	let page = 1;
 	let limit = 36;
 	let total = data.meta.total;
+	let layoutName: string = 'grid';
 	let loading = false;
 	let error = '';
 	let vtubers: Array<vtuberResponseData> = data.data;
@@ -94,24 +76,21 @@
 				<div class="w-full">
 					<InputSearch class="w-full" bind:value={name} placeholder="search vtuber name..." on:submit={onSubmit} />
 				</div>
-				{#each layouts as l, i}
-					{#if i === layout}
-						<div title={l.name + ' layout'} on:click={nextLayout} class="hover:opacity-70 cursor-pointer">
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-7 h-7">
-								<path fill-rule="evenodd" d={l.svg} clip-rule="evenodd" />
-							</svg>
-						</div>
-					{/if}
-				{/each}
+				<div title="advanced search" class="hover:opacity-70 cursor-pointer">
+					<AdvancedSearch />
+				</div>
+				<div>
+					<Layout bind:layoutName />
+				</div>
 			</div>
 		</div>
 	</div>
 	<Border class="col-span-6" />
 	{#each vtubers as vtuber}
-		{#if layout === 0}
+		{#if layoutName === 'grid'}
 			<VtuberGrid class="col-span-3 sm:col-span-2 md:col-span-1" id={vtuber.id} name={vtuber.name} image={vtuber.image} height={206} />
 		{/if}
-		{#if layout === 1}
+		{#if layoutName === 'card'}
 			<VtuberCard
 				class="col-span-6 sm:col-span-3 lg:col-span-2"
 				id={vtuber.id}
@@ -125,7 +104,7 @@
 				height={206}
 			/>
 		{/if}
-		{#if layout === 2}
+		{#if layoutName === 'list'}
 			<VtuberList
 				class="col-span-6"
 				id={vtuber.id}
---
+++ src/routes/vtubers/Layout.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	export let layoutName: string = 'grid';
+
+	const layouts = [
+		{
+			name: 'grid',
+			svg: 'M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z'
+		},
+		{
+			name: 'card',
+			svg: 'M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z'
+		},
+		{
+			name: 'list',
+			svg: 'M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z'
+		}
+	];
+
+	let layoutIndex: number = 0;
+
+	const nextLayout = () => {
+		layoutIndex = (layoutIndex + 1) % layouts.length;
+		layoutName = layouts[layoutIndex].name;
+	};
+</script>
+
+<div>
+	{#each layouts as l, i}
+		{#if i === layoutIndex}
+			<div title={l.name + ' layout'} on:click={nextLayout} class="hover:opacity-70 cursor-pointer">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7">
+					<path stroke-linecap="round" stroke-linejoin="round" d={l.svg} />
+				</svg>
+			</div>
+		{/if}
+	{/each}
+</div>
