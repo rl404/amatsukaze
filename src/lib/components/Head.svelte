9067d1e0cf120570c12f5034bc9e533d47e7ae60
axel <axel.rl.404@gmail.com>
2023-03-14 09:25:09 +0700

    fix: og:title

--- src/lib/components/Head.svelte
+++ src/lib/components/Head.svelte
@@ -7,7 +7,7 @@
 <svelte:head>
 	<title>{`${title} | Amatsukaze`}</title>
 	<meta name="description" content={description} />
-	<meta property="og:title" content="Amatsukaze - Vtuber Wikia Visualizer" />
+	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
 	<meta property="og:image" content={image} />
 	<meta property="og:type" content="website" />
