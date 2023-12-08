73952ffa180e7c285929c82604e5048783f42403
axel <axel.rl.404@gmail.com>
2023-12-08 16:44:19 +0700

    rewrite: init

--- src/lib/components/timelines/Timeline.svelte
+++
@@ -1,6 +0,0 @@
-<div class="relative">
-	<div class="absolute w-px h-full left-2 md:left-1/2 md:transform md:-translate-x-1/2 bg-neutral-300 dark:bg-neutral-600" />
-	<div class="grid gap-2 pl-2 md:pl-0">
-		<slot />
-	</div>
-</div>
