367e104405a29b52e00a3ac4892d6d3cee37497e
axel <axel.rl.404@gmail.com>
2023-12-22 15:02:39 +0700

    fix: reactive variable

--- src/routes/(app)/agencies/[id]/[...name]/MembersChart.svelte
+++ src/routes/(app)/agencies/[id]/[...name]/MembersChart.svelte
@@ -22,51 +22,55 @@
 	let minDate: Date = new Date();
 	let maxDate: Date = new Date();
 
-	data.forEach((vtuber) => {
-		if (vtuber.debut_date && new Date(vtuber.debut_date)) {
-			const debutDate = new Date(vtuber.debut_date);
-			if (debutDate < minDate) minDate = debutDate;
-			if (debutDate > maxDate) maxDate = debutDate;
+	$: data, setChartData();
+
+	const setChartData = () => {
+		data.forEach((vtuber) => {
+			if (vtuber.debut_date && new Date(vtuber.debut_date)) {
+				const debutDate = new Date(vtuber.debut_date);
+				if (debutDate < minDate) minDate = debutDate;
+				if (debutDate > maxDate) maxDate = debutDate;
+			}
+
+			if (vtuber.retirement_date && new Date(vtuber.retirement_date)) {
+				const retiredDate = new Date(vtuber.retirement_date);
+				if (retiredDate < minDate) minDate = retiredDate;
+				if (retiredDate > maxDate) maxDate = retiredDate;
+			}
+		});
+
+		minDate.setMonth(minDate.getMonth() - 1);
+		maxDate.setMonth(maxDate.getMonth() + 1);
+
+		while (minDate.toISOString().slice(0, 7) != maxDate.toISOString().slice(0, 7)) {
+			const key = `${minDate.toISOString().slice(0, 7)}-01`;
+			chartData[key] = { debut: 0, retired: 0, total: 0 };
+			minDate.setMonth(minDate.getMonth() + 1);
 		}
 
-		if (vtuber.retirement_date && new Date(vtuber.retirement_date)) {
-			const retiredDate = new Date(vtuber.retirement_date);
-			if (retiredDate < minDate) minDate = retiredDate;
-			if (retiredDate > maxDate) maxDate = retiredDate;
-		}
-	});
+		data.forEach((vtuber) => {
+			if (vtuber.debut_date && new Date(vtuber.debut_date)) {
+				const debutDate = new Date(vtuber.debut_date).toISOString();
+				const key = `${debutDate.slice(0, 7)}-01`;
+				chartData[key].debut++;
+				chartData[key].total++;
+			}
 
-	minDate.setMonth(minDate.getMonth() - 1);
-	maxDate.setMonth(maxDate.getMonth() + 1);
+			if (vtuber.retirement_date && new Date(vtuber.retirement_date)) {
+				const retiredDate = new Date(vtuber.retirement_date).toISOString();
+				const key = `${retiredDate.slice(0, 7)}-01`;
+				chartData[key].retired++;
+				chartData[key].total--;
+			}
+		});
 
-	while (minDate.toISOString().slice(0, 7) != maxDate.toISOString().slice(0, 7)) {
-		const key = `${minDate.toISOString().slice(0, 7)}-01`;
-		chartData[key] = { debut: 0, retired: 0, total: 0 };
-		minDate.setMonth(minDate.getMonth() + 1);
-	}
-
-	data.forEach((vtuber) => {
-		if (vtuber.debut_date && new Date(vtuber.debut_date)) {
-			const debutDate = new Date(vtuber.debut_date).toISOString();
-			const key = `${debutDate.slice(0, 7)}-01`;
-			chartData[key].debut++;
-			chartData[key].total++;
-		}
-
-		if (vtuber.retirement_date && new Date(vtuber.retirement_date)) {
-			const retiredDate = new Date(vtuber.retirement_date).toISOString();
-			const key = `${retiredDate.slice(0, 7)}-01`;
-			chartData[key].retired++;
-			chartData[key].total--;
-		}
-	});
-
-	Object.entries(chartData).forEach((d, i) => {
-		if (i === 0) return;
-		const key = d[0];
-		const prevKey = Object.entries(chartData)[i - 1][0];
-		chartData[key].total += chartData[prevKey].total;
-	});
+		Object.entries(chartData).forEach((d, i) => {
+			if (i === 0) return;
+			const key = d[0];
+			const prevKey = Object.entries(chartData)[i - 1][0];
+			chartData[key].total += chartData[prevKey].total;
+		});
+	};
 </script>
 
 <div class="grid gap-2">
