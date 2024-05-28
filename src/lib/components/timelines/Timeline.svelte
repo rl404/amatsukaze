8b5f199e249d5d194f05f413c1ee4e9e118f6f8e
axel <axel.rl.404@gmail.com>
2024-05-28 14:58:46 +0700

    rewrite: reset

--- src/lib/components/timelines/Timeline.svelte
+++
@@ -1,8 +0,0 @@
-<div class="relative">
-	<div
-		class="absolute left-2 h-full w-px bg-border dark:bg-border-dark md:left-1/2 md:-translate-x-1/2 md:transform"
-	/>
-	<div class="grid gap-2 pl-2 md:pl-0">
-		<slot />
-	</div>
-</div>
