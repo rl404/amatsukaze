6ac995f6f74e404b85b0695235d8e663516917fc
axel <axel.rl.404@gmail.com>
2024-06-30 12:20:24 +0700

    fix: error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Head from '$lib/components/commons/Head.svelte';
+
+	const status: number = $page.status;
+	const message: string = $page.error?.message || '';
+</script>
+
+<Head title={status.toString()} description={message} image="" />
+
+<div class="flex h-screen items-center justify-center text-center font-bold">
+	<div>
+		<h1 class="text-9xl">{status}</h1>
+		<h2>{message}</h2>
+	</div>
+</div>
