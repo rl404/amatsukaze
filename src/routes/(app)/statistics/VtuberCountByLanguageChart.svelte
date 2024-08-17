ae35df9231b467039de83abec457e989d1207ad5
axel <axel.rl.404@gmail.com>
2024-08-17 11:31:35 +0700

    feat: stats language

---
+++ src/routes/(app)/statistics/VtuberCountByLanguageChart.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import BarChart from '$lib/components/charts/BarChart.svelte';
+	import { getAxiosError } from '$lib/utils/api';
+	import axios from 'axios';
+	import { Spinner } from 'flowbite-svelte';
+	import { onMount } from 'svelte';
+	import type { VtuberLanguageCountResponseData } from '../../api/statistics/vtubers/language-count/+server';
+
+	export let limit: number = -1;
+
+	let data: VtuberLanguageCountResponseData[];
+	let loading: boolean = true;
+	let error: string = '';
+
+	onMount(() => {
+		axios
+			.get(`/api/statistics/vtubers/language-count`)
+			.then((resp) => (data = resp.data.data.slice(0, limit)))
+			.catch((err) => (error = getAxiosError(err)))
+			.finally(() => (loading = false));
+	});
+
+	const onClick = (d: any) =>
+		window.open(`/vtubers?language_id=${data[d.detail].id}`, '_blank')?.focus();
+</script>
+
+{#if loading}
+	<div class="flex h-full w-full items-center justify-center">
+		<Spinner />
+	</div>
+{:else if error !== ''}
+	<div class="flex h-full w-full items-center justify-center text-red-500">{error}</div>
+{:else}
+	<BarChart
+		data={data.map((d) => ({
+			name: d.name,
+			value: d.count
+		}))}
+		seriesName="Count"
+		on:clickArea={onClick}
+	/>
+{/if}
