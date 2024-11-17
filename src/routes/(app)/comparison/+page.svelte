0a92e5496ca50fd65786af54e3c1f92663c5fdf0
axel <axel.rl.404@gmail.com>
2024-11-17 10:58:17 +0700

    refactor: split comparison modal file

--- src/routes/(app)/comparison/+page.svelte
+++ src/routes/(app)/comparison/+page.svelte
@@ -1,43 +1,27 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import Head from '$lib/components/commons/Head.svelte';
 	import Image from '$lib/components/commons/Image.svelte';
 	import TextOutline from '$lib/components/commons/TextOutline.svelte';
 	import EditIcon from '$lib/components/icons/EditIcon.svelte';
-	import MinusIcon from '$lib/components/icons/MinusIcon.svelte';
 	import PlusIcon from '$lib/components/icons/PlusIcon.svelte';
 	import { getAxiosError } from '$lib/utils/api';
 	import { getWikiImg, mustArrayLen, toNumber, toURL } from '$lib/utils/utils';
 	import axios from 'axios';
-	import {
-		Breadcrumb,
-		BreadcrumbItem,
-		Button,
-		Card,
-		Modal,
-		Search,
-		Spinner,
-		Tooltip
-	} from 'flowbite-svelte';
+	import { Breadcrumb, BreadcrumbItem, Button, Card, Spinner, Tooltip } from 'flowbite-svelte';
 	import type { VtuberResponseData } from '../../api/vtubers/[id]/+server';
 	import BasicDetails from './BasicDetails.svelte';
 	import Medias from './Medias.svelte';
 	import PersonalDetails from './PersonalDetails.svelte';
+	import SearchModal from './SearchModal.svelte';
 
 	let ids: number[] = [0, 0, 0];
 	let loading: boolean[] = [false, false, false];
 	let error: string[] = ['', '', ''];
 	let data: VtuberResponseData[] | any[] = [{}, {}, {}];
 
-	let modal: boolean = false;
+	let open: boolean = false;
 	let modalData: { id: number; name: string; image: string }[] | any[] = [{}, {}, {}];
-
-	let delayTimer: number;
-	let searchQuery: string = '';
-	let searchLoading: boolean = false;
-	let searchError: string = '';
-	let searchData: VtuberResponseData[] = [];
 
 	$: $page && onURLChange();
 
@@ -80,47 +64,7 @@
 			name: d.name || '',
 			image: d.image || ''
 		}));
-		modal = !modal;
-	};
-
-	const onInput = () => {
-		searchData = [];
-		clearTimeout(delayTimer);
-		delayTimer = setTimeout(() => {
-			if (searchQuery.length < 3) return;
-			onSearch();
-		}, 500);
-	};
-
-	const onSearch = () => {
-		searchLoading = true;
-		searchError = '';
-		axios
-			.get(`/api/vtubers?names=${searchQuery}&limit=18`)
-			.then((resp) => (searchData = resp.data.data))
-			.catch((err) => (searchError = getAxiosError(err)))
-			.finally(() => (searchLoading = false));
-	};
-
-	const addVtuber = (vtuber: VtuberResponseData) => {
-		const exist = modalData.findIndex((d) => d.id === vtuber.id);
-		if (exist !== -1) return;
-
-		const i = modalData.findIndex((d) => d.id === 0);
-		modalData[i] = {
-			id: vtuber.id,
-			name: vtuber.name,
-			image: vtuber.image
-		};
-	};
-
-	const removeVtuber = (i: number) => {
-		modalData[i].id = 0;
-	};
-
-	const onCompare = () => {
-		modal = false;
-		goto(`?ids=${modalData.map((d) => d.id ?? 0).join(',')}`);
+		open = !open;
 	};
 </script>
 
@@ -153,7 +97,7 @@
 					</div>
 				{:else if error[i] !== ''}
 					<div class="flex aspect-square w-1/2 items-center justify-center text-red-500">
-						{searchError}
+						{error[i]}
 					</div>
 				{:else}
 					<Card
@@ -194,89 +138,4 @@
 	{/if}
 </div>
 
-<Modal title="Vtuber List" bind:open={modal} size="lg">
-	<div class="grid gap-4">
-		<div class="flex items-center justify-center gap-4">
-			{#each modalData as data, i}
-				{#if data.id === 0}
-					<Card class="aspect-square max-w-36"></Card>
-				{:else}
-					<Card
-						title={data.name}
-						size="none"
-						padding="none"
-						class="group relative aspect-square max-w-36 border-primary-500 hover:cursor-pointer"
-						on:click={() => removeVtuber(i)}
-					>
-						<Image
-							src={getWikiImg(data.image)}
-							alt={data.name}
-							class="h-full w-full rounded-lg object-cover object-top"
-						/>
-						<div
-							class="h6 pointer-events-none absolute bottom-0 line-clamp-1 w-full rounded-b-lg bg-primary-500 p-0.5 text-center text-white"
-						>
-							{data.name}
-						</div>
-						<div
-							class="absolute right-1 top-1 opacity-0 transition-opacity group-hover:opacity-100"
-						>
-							<Button class="!p-2" pill color="red">
-								<MinusIcon class="size-3" />
-							</Button>
-						</div>
-					</Card>
-				{/if}
-			{/each}
-		</div>
-		<div>
-			<Search
-				size="md"
-				placeholder="vtuber name..."
-				disabled={searchLoading}
-				bind:value={searchQuery}
-				on:input={onInput}
-			/>
-		</div>
-		<div class="grid grid-cols-6 gap-2">
-			{#if searchLoading}
-				<div class="col-span-6 text-center"><Spinner /></div>
-			{:else if searchError !== ''}
-				<div class="col-span-6 text-center text-red-500">{searchError}</div>
-			{:else if searchQuery !== '' && searchData.length === 0}
-				<div class="col-span-6 text-center">No result...</div>
-			{:else}
-				{#each searchData as vtuber}
-					<Card
-						title={vtuber.name}
-						size="none"
-						padding="none"
-						class="group relative aspect-square h-full w-full transition hover:cursor-pointer hover:!border-primary-500"
-						on:click={() => addVtuber(vtuber)}
-					>
-						<Image
-							src={getWikiImg(vtuber.image)}
-							alt={vtuber.name}
-							class="h-full w-full rounded-lg object-cover object-top"
-						/>
-						<div
-							class="h6 pointer-events-none absolute bottom-0 line-clamp-1 w-full rounded-b-lg bg-primary-500 p-0.5 text-center text-white opacity-0 transition-opacity group-hover:opacity-100"
-						>
-							{vtuber.name}
-						</div>
-						<div
-							class="absolute right-1 top-1 opacity-0 transition-opacity group-hover:opacity-100"
-						>
-							<Button class="!p-2" pill>
-								<PlusIcon class="size-3" />
-							</Button>
-						</div>
-					</Card>
-				{/each}
-			{/if}
-		</div>
-	</div>
-	<div slot="footer" class="flex w-full items-start justify-end">
-		<Button on:click={onCompare}>Compare</Button>
-	</div>
-</Modal>
+<SearchModal bind:open bind:modalData />
---
+++ src/routes/(app)/comparison/SearchModal.svelte
@@ -0,0 +1,147 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import Image from '$lib/components/commons/Image.svelte';
+	import MinusIcon from '$lib/components/icons/MinusIcon.svelte';
+	import PlusIcon from '$lib/components/icons/PlusIcon.svelte';
+	import { getAxiosError } from '$lib/utils/api';
+	import { getWikiImg } from '$lib/utils/utils';
+	import axios from 'axios';
+	import { Button, Card, Modal, Search, Spinner } from 'flowbite-svelte';
+	import type { VtuberResponseData } from '../../api/vtubers/[id]/+server';
+
+	export let open: boolean;
+	export let modalData: { id: number; name: string; image: string }[] | any[];
+
+	let delayTimer: number;
+	let query: string = '';
+	let loading: boolean = false;
+	let error: string = '';
+	let vtubers: VtuberResponseData[] = [];
+
+	const onInput = () => {
+		vtubers = [];
+		clearTimeout(delayTimer);
+		delayTimer = setTimeout(() => {
+			if (query.length < 3) return;
+			onSearch();
+		}, 500);
+	};
+
+	const onSearch = () => {
+		loading = true;
+		error = '';
+		axios
+			.get(`/api/vtubers?names=${query}&limit=18`)
+			.then((resp) => (vtubers = resp.data.data))
+			.catch((err) => (error = getAxiosError(err)))
+			.finally(() => (loading = false));
+	};
+
+	const addVtuber = (vtuber: VtuberResponseData) => {
+		const exist = modalData.findIndex((d) => d.id === vtuber.id);
+		if (exist !== -1) return;
+
+		const i = modalData.findIndex((d) => d.id === 0);
+		modalData[i] = {
+			id: vtuber.id,
+			name: vtuber.name,
+			image: vtuber.image
+		};
+	};
+
+	const removeVtuber = (i: number) => {
+		modalData[i].id = 0;
+	};
+
+	const onCompare = () => {
+		open = false;
+		goto(`?ids=${modalData.map((d) => d.id ?? 0).join(',')}`);
+	};
+</script>
+
+<Modal title="Vtuber List" bind:open size="lg">
+	<div class="grid gap-4">
+		<div class="flex items-center justify-center gap-4">
+			{#each modalData as data, i}
+				{#if data.id === 0}
+					<Card class="aspect-square max-w-36"></Card>
+				{:else}
+					<Card
+						title={data.name}
+						size="none"
+						padding="none"
+						class="group relative aspect-square max-w-36 border-primary-500 hover:cursor-pointer"
+						on:click={() => removeVtuber(i)}
+					>
+						<Image
+							src={getWikiImg(data.image)}
+							alt={data.name}
+							class="h-full w-full rounded-lg object-cover object-top"
+						/>
+						<div
+							class="h6 pointer-events-none absolute bottom-0 line-clamp-1 w-full rounded-b-lg bg-primary-500 p-0.5 text-center text-white"
+						>
+							{data.name}
+						</div>
+						<div
+							class="absolute right-1 top-1 opacity-0 transition-opacity group-hover:opacity-100"
+						>
+							<Button class="!p-2" pill color="red">
+								<MinusIcon class="size-3" />
+							</Button>
+						</div>
+					</Card>
+				{/if}
+			{/each}
+		</div>
+		<div>
+			<Search
+				size="md"
+				placeholder="vtuber name..."
+				disabled={loading}
+				bind:value={query}
+				on:input={onInput}
+			/>
+		</div>
+		<div class="grid grid-cols-6 gap-2">
+			{#if loading}
+				<div class="col-span-6 text-center"><Spinner /></div>
+			{:else if error !== ''}
+				<div class="col-span-6 text-center text-red-500">{error}</div>
+			{:else if query !== '' && vtubers.length === 0}
+				<div class="col-span-6 text-center">No result...</div>
+			{:else}
+				{#each vtubers as vtuber}
+					<Card
+						title={vtuber.name}
+						size="none"
+						padding="none"
+						class="group relative col-span-2 aspect-square h-full w-full transition hover:cursor-pointer hover:!border-primary-500 sm:col-span-1"
+						on:click={() => addVtuber(vtuber)}
+					>
+						<Image
+							src={getWikiImg(vtuber.image)}
+							alt={vtuber.name}
+							class="h-full w-full rounded-lg object-cover object-top"
+						/>
+						<div
+							class="h6 pointer-events-none absolute bottom-0 line-clamp-1 w-full rounded-b-lg bg-primary-500 p-0.5 text-center text-white opacity-0 transition-opacity group-hover:opacity-100"
+						>
+							{vtuber.name}
+						</div>
+						<div
+							class="absolute right-1 top-1 opacity-0 transition-opacity group-hover:opacity-100"
+						>
+							<Button class="!p-2" pill>
+								<PlusIcon class="size-3" />
+							</Button>
+						</div>
+					</Card>
+				{/each}
+			{/if}
+		</div>
+	</div>
+	<div slot="footer" class="flex w-full items-start justify-end">
+		<Button on:click={onCompare}>Compare</Button>
+	</div>
+</Modal>
