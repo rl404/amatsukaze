2ee909dc9ecce30bd263a1735b6eb519cc09a628
axel <axel.rl.404@gmail.com>
2025-07-17 10:57:34 +0700

    update: vercel speed insight

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import PageTransition from '$lib/components/commons/PageTransition.svelte';
 	import modeObserver from '$lib/utils/modeObserver';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import { onMount } from 'svelte';
 	import '../app.css';
 
 	let { children } = $props();
 
 	onMount(modeObserver);
+	injectSpeedInsights();
 </script>
 
 <PageTransition />
