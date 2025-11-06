c08a9ab04aaff50ed0c56ad6e62f23062b246393
Axel <axel.rl.404@gmail.com>
2025-11-06 16:54:05 +0700

    feat: vtuber list background

--- src/lib/components/layouts/VtuberList.svelte
+++ src/lib/components/layouts/VtuberList.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Image from '$lib/components/commons/Image.svelte';
 	import RenderIfVisible from '$lib/components/commons/RenderIfVisible.svelte';
 	import { getWikiImg, toURL } from '$lib/utils/utils';
 	import { Avatar, Badge, Card, Tooltip } from 'flowbite-svelte';
@@ -17,25 +18,37 @@
 	<Card
 		title={name}
 		href="/vtubers/{id}/{toURL(name)}"
-		class="grid max-w-full grid-cols-10 items-center gap-2 p-2 text-center"
+		class="relative grid max-w-full grid-cols-10 items-center gap-2 overflow-hidden p-2 text-center"
 	>
-		<div class="col-span-10 flex items-center gap-2 sm:col-span-5 md:col-span-3">
+		{#if image}
+			<div class="aspect-portrait absolute -top-2/3 right-0 h-60 opacity-15 dark:opacity-10">
+				<Image
+					src={getWikiImg(image)}
+					alt={name}
+					class="aspect-portrait absolute top-0 right-0 h-full bg-none object-cover object-top"
+				/>
+				<div
+					class="absolute top-0 right-0 size-full bg-gradient-to-r from-white to-transparent group-hover:from-gray-100 dark:from-gray-800 dark:group-hover:from-gray-700"
+				/>
+			</div>
+		{/if}
+		<div class="z-10 col-span-10 flex items-center gap-2 sm:col-span-5 md:col-span-3">
 			<Avatar src={getWikiImg(image)} alt={name} class="size-7 object-cover object-top" />
 			<h4 class="h5 line-clamp-1 text-left">{name}</h4>
 		</div>
 		<div
-			class="col-span-5 hidden text-right sm:line-clamp-1 md:col-span-3 md:text-center xl:col-span-2"
+			class="z-10 col-span-5 hidden text-right sm:line-clamp-1 md:col-span-3 md:text-center xl:col-span-2"
 			title={agencies && agencies.join(', ')}
 		>
 			{agencies.length === 0 ? '-' : agencies.join(', ')}
 		</div>
-		<div class="col-span-2 hidden md:block" title="debut date">
+		<div class="z-10 col-span-2 hidden md:block" title="debut date">
 			{!debutDate ? '-' : debutDate.toISOString().slice(0, 10)}
 		</div>
-		<div class="col-span-2 hidden md:block" title="retirement date">
+		<div class="z-10 col-span-2 hidden md:block" title="retirement date">
 			{!retirementDate ? '-' : retirementDate.toISOString().slice(0, 10)}
 		</div>
-		<div class="hidden items-center justify-center gap-1 xl:flex">
+		<div class="z-10 hidden items-center justify-center gap-1 xl:flex">
 			{#if has2d}
 				<Badge color="pink">2D</Badge>
 				<Tooltip>has 2D model</Tooltip>
